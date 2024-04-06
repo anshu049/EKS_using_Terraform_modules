11360facae5cdc7444587aba68930f962dfa6a52
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-04-06 21:40:21 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "dev-remote-state-bucketttt"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "state-locking"
-  }
-}
